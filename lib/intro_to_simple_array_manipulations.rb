7f582add6a5be003f1fb63af85b74c45f864af57
Daniel Madera <dmadera0@gmail.com>
2020-06-09 15:19:04 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -10,5 +10,12 @@ def using.pop(array)
   array.pop
 end
 
-def pop_with_args()
-  
+def pop_with_args(array)
+  array.pop(2)
+end
+
+def using_shift(array,string)
+  array.shift
+end
+
+def shift_with_args
